efddb4649f3fc7d9708b3579bf06acdfef6f31de
lcwzz <1074699725@qq.com>
2021-04-08 18:02:23 +0800

    医务人员考勤信息功能完成

--- src/components/user/Record.vue
+++ src/components/user/Record.vue
@@ -1,15 +1,77 @@
 <template>
   <div>
-    今日考勤
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>考勤打卡</span>
+      </div>
+      <div class="text">
+        {{time}}
+      </div>
+      <div style="margin-top: 20px">
+        <el-button type="primary" @click="clockClick('上班')">上班打卡</el-button>
+        <el-button type="success" @click="clockClick('下班')">下班打卡</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
+let moment = require("moment");
 export default {
-  name: "Record"
+  name: "Record",
+  data() {
+    return {
+      time: "",
+    }
+  },
+  created() {
+    setInterval(this.myTimer, 1000);
+  },
+  methods: {
+    myTimer() {
+      this.time = moment().format("YYYY-MM-DD HH:mm:ss");
+    },
+    clockClick(symbol) {
+      const doctor = JSON.parse(sessionStorage.getItem("user"));
+      var data = {
+        'id': doctor.id,
+        'time': this.time,
+        'symbol': symbol
+      };
+      this.$http.post("http://localhost/doctor/signIn", data).then(response => {
+        console.log(response.data);
+        let res = response.data;
+        if (!res.success) {
+          this.$message.error(res.message);
+        } else {
+          this.$message.success("打卡成功!");
+        }
+      });
+    },
+  }
 }
 </script>
 
 <style scoped>
+.text {
+  font-size: 30px;
+}
 
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+
+.box-card {
+  width: 480px;
+}
 </style>
--- src/components/user/Records.vue
+++ src/components/user/Records.vue
@@ -1,12 +1,34 @@
 <template>
-<div>
-  查看考勤
-</div>
+  <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column type="index" label="序号" width="60px"></el-table-column>
+    <el-table-column prop="time" label="打卡时间"></el-table-column>
+    <el-table-column prop="state" label="打卡状态"></el-table-column>
+  </el-table>
 </template>
 
 <script>
 export default {
-  name: "Records"
+  name: "Records",
+  data() {
+    return {
+      tableData: []
+    }
+  },
+  created() {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    this.$http.get("http://localhost/doctor/findRecords?id=" + user.id).then(response => {
+      console.log(response.data);
+      let res = response.data;
+      if (!res.success) {
+        this.$message.error(res.message);
+      } else {
+        this.tableData = res.data;
+      }
+    });
+  }
 }
 </script>
 
